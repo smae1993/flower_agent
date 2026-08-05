17c5c8259fa508f8967b08a14605e123f05839b6
Sayed Mohammad Amin Emrani <smae1993@gmail.com>
2026-08-05 10:11:32 +0330

    feat: export project map APIs

--- packages/flower_core/lib/flower_core.dart
+++ packages/flower_core/lib/flower_core.dart
@@ -3,4 +3,6 @@ library;
 export 'src/flower_exception.dart';
 export 'src/project_initializer.dart';
 export 'src/project_inspector.dart';
+export 'src/project_map.dart';
+export 'src/project_mapper.dart';
 export 'src/project_snapshot.dart';
